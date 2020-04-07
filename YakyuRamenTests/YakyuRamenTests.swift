1c546b76cc3e27a561898b6430943e342bab444c
gmmoraes <gabrielgmmoraes@gmail.com>
2020-04-06 23:04:36 -0300

    Testing snapshot

---
+++ YakyuRamenTests/YakyuRamenTests.swift
@@ -0,0 +1,154 @@
+//
+//  YakyuRamenTests.swift
+//  YakyuRamenTests
+//
+//  Created by Gabriel on 18/01/20.
+//  Copyright © 2020 Gabriel. All rights reserved.
+//
+
+import SnapshotTesting
+import XCTest
+
+@testable import Yakyu_Ramen
+
+class YakyuRamenTests: XCTestCase {
+    
+    let leagueTableView = LeagueTableViewController()
+    let launchScreenVC = LaunchScreenViewController()
+    //let appBundle = Bundle(for: type(of: LaunchScreenViewController) as! LaunchScreenViewController)
+
+    override func setUp() {
+              
+          
+        
+        
+//        launchScreenVC.loadView()
+//        let _ = launchScreenVC.view
+       // launchScreenVC.viewDidLoad()
+        
+//        leagueTableView.viewDidLoad()
+//        leagueTableView.loadView()
+//        let _ = leagueTableView.view
+        
+        
+
+//        // Test and Load the View at the Same Time!
+//        XCTAssertNotNil(navigationController.view)
+//        XCTAssertNotNil(currentViewController.view)
+    }
+    
+    let devices = [("iPhoneX_portrait", ViewImageConfig.iPhoneX(.portrait)),
+                   ("iPhoneSe_portrait", ViewImageConfig.iPhoneSe(.portrait)),
+                   ("iPhone8Plus_portrait", ViewImageConfig.iPhone8Plus(.portrait)),
+
+                   ("iPhoneXsMax_portrait", ViewImageConfig.iPhoneXsMax(.portrait))]
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testViewInMultipleDevices(viewController: UIViewController, devices: [(String, ViewImageConfig)]) {
+        for (name, config) in devices {
+            assertSnapshot(matching: viewController, as: .image(on: config), named: name)
+        }
+    }
+    
+    func testLaunchScreenViewController() {
+        let formViewController = UIStoryboard.init(name: "Main", bundle: nil).instantiateInitialViewController() as! LaunchScreenViewController
+        formViewController.view.layoutSubviews() //init outlets
+        
+        UIScreen.main.setValue(2.0, forKeyPath: "scale")
+        testViewInMultipleDevices(viewController: formViewController, devices: devices)
+    }
+    
+    func testView() {
+      //record = true
+        
+        let storyboard = UIStoryboard(name: "Main",
+                                      bundle: Bundle.main)
+        let currentViewController = storyboard.instantiateInitialViewController() as! LaunchScreenViewController
+
+        currentViewController.loadView()
+        let _ = currentViewController.view
+        
+      
+        currentViewController.beginAppearanceTransition(true, animated: true)
+
+        
+        assertSnapshot(matching: currentViewController, as: .image, record: false)
+        assertSnapshot(matching: currentViewController, as: .image(on: .iPhoneXsMax))
+        currentViewController.endAppearanceTransition()
+        //assertSnapshot(matching: launchScreenVC, as: .image(on: .iPhoneXsMax))
+        //assertSnapshot(matching: launchScreenVC.view, as: .image(on: .iPhoneSe))
+        
+        
+    }
+    
+    func testStoredView() {
+
+        let storyboard = UIStoryboard(name: "Main",
+                                      bundle: Bundle.main)
+        let navController =  storyboard.instantiateViewController(identifier: "pacificLeagueTable") as! UINavigationController
+        
+        if let currentViewController = navController.topViewController {
+            
+                //pacificLeagueTable
+            currentViewController.loadView()
+            let _ = currentViewController.view
+            
+            
+            currentViewController.beginAppearanceTransition(true, animated: true)
+
+            
+            assertSnapshot(matching: currentViewController, as: .image, record: false)
+            currentViewController.endAppearanceTransition()
+            
+        }
+        
+
+        //assertSnapshot(matching: launchScreenVC, as: .image(on: .iPhoneXsMax))
+        //assertSnapshot(matching: launchScreenVC.view, as: .image(on: .iPhoneSe))
+        
+        
+    }
+    
+    func testLeagueTableView() {
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard
+            .instantiateViewController(withIdentifier: "tabBarController") as! UITabBarController
+
+        viewController.beginAppearanceTransition(true, animated: false)
+        
+        //assertSnapshot(matching: viewController, as: .image, record: false)
+        UIScreen.main.setValue(2.0, forKeyPath: "scale")
+        testViewInMultipleDevices(viewController: viewController, devices: devices)
+        viewController.endAppearanceTransition()
+    }
+
+    func testCentralLeagueTableView() {
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard
+            .instantiateViewController(withIdentifier: "tabBarController") as! UITabBarController
+        
+        viewController.view.layoutSubviews()
+        //UIScreen.main.setValue(2.0, forKeyPath: "scale")
+        viewController.beginAppearanceTransition(true, animated: false)
+        
+        //assertSnapshot(matching: viewController, as: .image, record: false)
+        
+        //let device = devices
+        for (name, config) in devices {
+            assertSnapshot(matching: viewController, as: .image(on: config, precision: 1.00), named: name)
+        }
+        viewController.endAppearanceTransition()
+    }
+
+
+}
